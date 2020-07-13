ebf9b7d1e23eb60369a47d2c08446345f1459c5c
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-07-13 14:09:01 -0500

    Alterado o formulário padrão p/ biblioteca UnForm

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,18 +1,36 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input {...rest} />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef(null);
+  /** Recebe os dados do formulário */
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  /** Quando o componente for exibido em tela, o registerField será chamado recebendo
+   * algumas propriedades.
+   */
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      {Icon && <Icon size={20} />}
+      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+    </Container>
+  );
+};
 
 export default Input;
