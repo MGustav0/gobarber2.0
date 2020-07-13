73f0989afac46c4f751111f12b3161b07ffd34ab
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-07-13 16:33:45 -0500

    Tooltip de erros

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -22,6 +25,12 @@ export const Container = styled.div<ContainerProps>`
   }
 
   /* Acessa as propriedades do componente */
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
   ${props =>
     props.isFocused &&
     css`
@@ -49,5 +58,24 @@ export const Container = styled.div<ContainerProps>`
   /* Pega o primeiro SVG e aplica o estilo */
   > svg {
     margin-right: 16px;
+  }
+`;
+
+/* Herança de estilização, estiliza outro componente */
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
