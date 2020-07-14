702e5201d394c51b51e063a03ba4619c751cc150
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-07-13 19:34:58 -0500

    Usuário e Token são enviados ao localStorage

--- web/src/context/AuthContext.tsx
+++ web/src/context/AuthContext.tsx
@@ -1,6 +1,11 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 
 import api from '../services/apiClient';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
 
 interface SignInCredentials {
   email: string;
@@ -8,7 +13,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  name: string;
+  user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
@@ -18,15 +23,37 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
  * AuthContext.Provider
  */
 const AuthProvider: React.FC = ({ children }) => {
+  /** Busca os dados de login no localStorage, executa apenas em caso de refresh ou
+   * quando o usuário fechar a aplicação
+   */
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+    /** Caso não encontre o localStorage com os campos vazios, não retorna nada */
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
       password,
     });
+
+    const { token, user } = response.data;
+
+    /** Armazena o token no localStorage */
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Gustavo', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
