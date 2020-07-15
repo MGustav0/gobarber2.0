0e284e9204a76afdc6676a769f3c0388677895c4
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-07-15 16:31:01 -0500

    Cadastro concluído

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/mobile';
 import * as Yup from 'yup';
 
+import api from '../../services/api';
 import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
@@ -55,9 +56,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação.',
+      );
+
+      navigation.goBack();
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         const errors = getValidationErrors(error);
@@ -69,8 +75,8 @@ const SignUp: React.FC = () => {
 
       /** Dispara uma mensagem de erro */
       Alert.alert(
-        'Erro na autenticação',
-        'Ocorreu um erro ao fazer login, cheque as credenciais',
+        'Erro no cadastro',
+        'Ocorreu um erro ao fazer o cadastro, tente novamente',
       );
     }
   }, []);
