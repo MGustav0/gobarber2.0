0cd14db31b2980482140affe77c7296d4d32f565
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-07-23 12:48:33 -0500

    Invalidação de cache concluída

--- backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -30,4 +30,17 @@ export default class RedisCacheProvider implements ICacheProvider {
   }
 
   public async invalidate(key: string): Promise<void> {}
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    /** Deleta/invalida os ids de usuários salvos */
+    const keys = await this.client.keys(`${prefix}:*`);
+
+    const pipeline = this.client.pipeline();
+
+    keys.forEach(key => {
+      pipeline.del(key);
+    });
+
+    await pipeline.exec();
+  }
 }
--- backend/src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ backend/src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -2,4 +2,5 @@ export default interface ICacheProvider {
   save(key: string, value: any): Promise<void>;
   recover<TypeArgument>(key: string): Promise<TypeArgument | null>;
   invalidate(key: string): Promise<void>;
+  invalidatePrefix(prefix: string): Promise<void>;
 }
