fd5e5a61b19d103637db2d8662ddcc4955ddf8ce
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-07-27 00:13:58 -0500

    Recuperação de senha concluída

--- web/src/components/Button/index.tsx
+++ web/src/components/Button/index.tsx
@@ -2,15 +2,17 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
 /** Recupera o texto através da propriedade "children", e o restante das propriedades
  * coloca-se na variável "...rest"
  */
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
   <Container type="button" {...rest}>
-    {children}
+    {loading ? 'Enviando...' : children}
   </Container>
 );
 
