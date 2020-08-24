2c61a61a2ee94858128ee4e4b2b246b6a079a457
Gustavo Moreira <18315899+MGustav0@users.noreply.github.com>
2020-08-24 00:08:15 -0500

    Rotas e páginas criadas

---
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import styles from './styles';
+
+const AppointmentCreated: React.FC = () => {
+  return <View />;
+};
+
+export default AppointmentCreated;
